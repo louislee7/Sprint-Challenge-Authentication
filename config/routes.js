492b48de39d5fb17bffa441fa8aad9f3b5b46ac7
louislee7 <7.louislee@gmail.com>
2019-01-26 09:36:42 -0800

    register endpoint done

--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,9 @@
 const axios = require('axios');
+const bcrypt = require('bcryptjs');
+const knex = require('knex');
+const dbConfig = require('../knexfile');
+
+const db = knex(dbConfig.development);
 
 const { authenticate } = require('../auth/authenticate');
 
@@ -10,6 +15,20 @@ module.exports = server => {
 
 function register(req, res) {
   // implement user registration
+  const creds = req.body;
+  if (creds.username && creds.password) {
+    creds.password = bcrypt.hashSync(creds.password, 12);
+    db('user').insert(creds)
+      .then(ids => {
+        const id = ids[0];
+        res.status(201).json(id);
+      })
+      .catch(err => {
+        res.status(500).send("Failed to register a new user.");
+      })
+  } else {
+    res.status(400).send("Please provide a username and a password.")
+  }
 }
 
 function login(req, res) {
